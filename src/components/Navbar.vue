e3e7c1f1938218d07e3b88f116cad6db0e910dc2
cypherfunk-dev <andrescarlos2211@gmail.com>
2025-11-29 15:54:09 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+        <v-app-bar :elevation="2">
+
+            <v-app-bar-title class="ml-4">
+                <span class="text-green-accent-3 font-weight-bold mr-1">
+                    &gt;
+                </span>
+                <span>Diario de un soporte</span>
+
+            </v-app-bar-title>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <!-- <v-spacer></v-spacer> -->
+
+            <v-app-bar-nav-icon class="mr-4"></v-app-bar-nav-icon>
+        </v-app-bar>
+    </v-app>
+</template>
